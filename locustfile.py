ac5c2eaed7f9e5f3a6d18d56252e0e6a043942e3
Oleksandr Kaminskyi (EXT-Nokia) <oleksandr.kaminskyi.ext@nokia.com>
2026-03-24 16:53:44 +0200

    Debug CI

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,8 @@
 from locust import HttpUser, between
+from tasks.auth_tasks import AuthTasks
 from tasks.mixed_load import MixedLoadTasks
+from tasks.component_tasks import ComponentTasks
 
 class ApiUser(HttpUser):
     wait_time = between(1, 3)
-    tasks = [MixedLoadTasks]
+    tasks = [MixedLoadTasks, ComponentTasks, AuthTasks]
