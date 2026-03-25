6629c8ab7f976698a360ff19ba75fc0f31a20a56
Oleksandr Kaminskyi (EXT-Nokia) <oleksandr.kaminskyi.ext@nokia.com>
2026-03-25 18:44:59 +0200

    Fix type issue

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_URL = str(os.getenv("BASE_URL"))
+BASE_URL = os.getenv("BASE_URL", "http://eaapi.somee.com/")
 AUTH_USER_NAME = os.getenv("AUTH_USER_NAME")      
 AUTH_PASSWORD = os.getenv("AUTH_PASSWORD")
 
