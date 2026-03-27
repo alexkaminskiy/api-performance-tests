417f11ab0e6e5a9fb597823312a37cc94a4bd0c5
Oleksandr Kaminskyi (EXT-Nokia) <oleksandr.kaminskyi.ext@nokia.com>
2026-03-27 13:26:07 +0200

    debug hide sensitive vars

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL", "http://eaapi.somee.com/")
-AUTH_USER_NAME = os.getenv("AUTH_NAME")      
+AUTH_USER_NAME = os.getenv("AUTH_USER_NAME")      
 AUTH_PASSWORD = os.getenv("AUTH_PASSWORD")
 
 DEFAULT_PRODUCT_ID = 1
